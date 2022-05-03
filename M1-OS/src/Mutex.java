03dcefc2b8bc6f29d8cd226ac13c323cd4684efc
mina0magdy <92601097+mina0magdy@users.noreply.github.com>
2022-05-03 12:35:43 +0200

    Adding semWait and semSignal

--- M1-OS/src/Mutex.java
+++ M1-OS/src/Mutex.java
@@ -1,5 +1,30 @@
 public class Mutex {
-    public Mutex(){
+    private boolean locked ;
+    private int lockingProcessId ;
 
+    public Mutex() {
+        locked = false;
+        lockingProcessId = -1;
+    }
+
+    public void semWait(OsProcess process) {
+        locked = true;
+        lockingProcessId = process.getProcessId();
+    }
+
+    public void semSignal(OsProcess process) {
+        if(lockingProcessId == process.getProcessId()) {
+            locked = false;
+        }
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public int getLockingProcessId() {
+        return lockingProcessId;
     }
 }
+
+
